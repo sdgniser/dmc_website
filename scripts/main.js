aa4248a412fd6ffbb4d0450f645b9614ef8c1480
Ayush Singh <ayush.singh@niser.ac.in>
2019-01-30 22:57:43 +0530

    Landing page: added blur effect, social media icons

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,47 @@
 let header = document.querySelector('#ttl');
+let introTile = document.querySelector('#intro');
 let bg = document.querySelector('.backgroundimagehack');
+let fb = document.querySelector('#social .fb a');
+let yt = document.querySelector('#social .yt a');
+let ig = document.querySelector('#social .ig a');
+
+function turnSocialOn() {
+	fb.classList.add('on');
+	yt.classList.add('on');
+	ig.classList.add('on');
+}
+
+function turnSocialOff() {
+	fb.classList.remove('on');
+	yt.classList.remove('on');
+	ig.classList.remove('on');
+}
 
 function blurBg() {
 	bg.classList.add('blurimage');
+
 }
 
 function unblurBg() {
 	bg.classList.remove('blurimage');
 }
 
-header.addEventListener('mouseover', blurBg);
-header.addEventListener('mouseout', unblurBg);
+header.addEventListener('mouseover', function() {
+	blurBg();
+	turnSocialOn();
+});
+
+header.addEventListener('mouseout', function() {
+	unblurBg();
+	turnSocialOff()
+});
+
+fb.addEventListener('mouseover', blurBg);
+fb.addEventListener('mouseout', unblurBg);
+
+yt.addEventListener('mouseover', blurBg);
+yt.addEventListener('mouseout', unblurBg);
+
+ig.addEventListener('mouseover', blurBg);
+ig.addEventListener('mouseout', unblurBg);
+
