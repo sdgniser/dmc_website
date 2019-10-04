0a525d797df79af81543b102e500150094b71000
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2019-10-05 01:52:03 +0530

    Code Cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -368,15 +368,16 @@ yt.addEventListener('mouseout', unblurBg);
 ig.addEventListener('mouseover', blurBg);
 ig.addEventListener('mouseout', unblurBg);
 
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".pre-loader").length > 0)
-        {
-            $(".pre-loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
+// PERF NERF
+// (function($){
+//   'use strict';
+//     $(window).on('load', function () {
+//         if ($(".pre-loader").length > 0)
+//         {
+//             $(".pre-loader").fadeOut("slow");
+//         }
+//     });
+// })(jQuery)
 
 
 // let t = document.querySelector('.tiles');
@@ -430,41 +431,12 @@ $(function() {
 		easing: "easeOutExpo",
 		touchScroll: true,
 		setHeights: false,
-		standardScrollElements: '.timeline',
-		offset: -8,
+		// standardScrollElements: '.timeline', // Un-comment this to make .timeline scrollable - breaks Scrollify's scrolling though
+		offset: -8
 	});
   });
 
-// PRELOADER - Wrap every letter in a span
-$('.ml1 .letters').each(function(){
-	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-	});
-
-anime.timeline({loop: false})
-.add({
-	targets: '.ml1 .letter',
-	scale: [0.3,1],
-	opacity: [0,1],
-	translateZ: 0,
-	easing: "easeOutExpo",
-	duration: 600,
-	delay: function(el, i) {
-	return 70 * (i+1)
-	}
-}).add({
-	targets: '.ml1 .line',
-	scaleX: [0,1],
-	opacity: [0.5,1],
-	easing: "easeOutExpo",
-	duration: 700,
-	offset: '-=875',
-	delay: function(el, i, l) {
-	return 80 * (l - i);
-	}
-}).add({
-	targets: '.ml1',
-	opacity: 0,
-	duration: 1000,
-	easing: "easeOutExpo",
-	delay: 1000
-});
+// PRELOADER - Wrap every letter in a span -- PERF NERF
+// $('.ml1 .letters').each(function(){
+// 	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+// 	});
