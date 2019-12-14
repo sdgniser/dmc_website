796aef3d27af24db3db32fc773f8874cfd0fbd2e
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2019-12-14 15:09:12 +0530

    GALLERY - DRAGGABLE NOW

--- scripts/main.js
+++ scripts/main.js
@@ -69,6 +69,35 @@ const gallery_image_resizer = function() {
 
 gallery_image_resizer();
 
+// From here: https://codepen.io/toddwebdev/pen/yExKoj
+const slider = document.querySelector('.gallery-slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+
 // function disableScrollify(toggle){
 // 	if(toggle){
 // 		$.scrollify.destroy();
